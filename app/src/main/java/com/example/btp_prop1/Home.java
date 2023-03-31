aeafba72a2e1f01e6a364b69fa0c9d4b63e0cc8b
btpssss <btpsssslnmiit@gmail.com>
2023-03-31 22:53:53 +0530

    menu change

--- app/src/main/java/com/example/btp_prop1/Home.java
+++ app/src/main/java/com/example/btp_prop1/Home.java
@@ -1,5 +1,6 @@
 package com.example.btp_prop1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -8,54 +9,43 @@ import androidx.fragment.app.FragmentTransaction;
 import android.annotation.SuppressLint;
 import android.media.Image;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class Home extends AppCompatActivity {
 
-    ImageButton homebutton;
-    ImageButton calendarbutton;
-    ImageButton clockbutton;
-    ImageButton profilebutton;
+    private BottomNavigationView bottomNavigationView;
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        homebutton = (ImageButton) findViewById(R.id.home_imagebutton);
-        calendarbutton = (ImageButton) findViewById(R.id.calendar_imagebutton);
-        clockbutton = (ImageButton) findViewById(R.id.clock_imagebutton);
-        profilebutton=(ImageButton) findViewById(R.id.profile_imagebutton);
 
-        homebutton.setOnClickListener(new View.OnClickListener() {
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        replaceFragment(new HomeFragment());
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                replaceFragment(new HomeFragment());
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_schedule:
+                        replaceFragment(new CalenderFragment());
+                        return true;
+                    case R.id.navigation_clock:
+                        replaceFragment(new ClockFragment());
+                        return true;
+                    case R.id.navigation_profiles:
+                        replaceFragment(new ProfileFragment());
+                        return true;
+                    default:
+                        replaceFragment(new HomeFragment());
+                        return true;
+                }
             }
         });
-
-
-        calendarbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                replaceFragment(new CalenderFragment());
-            }
-        });
-
-        clockbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                replaceFragment(new ClockFragment());
-            }
-        });
-
-        profilebutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                replaceFragment(new ProfileFragment());
-            }
-        });
-
     }
 
     private void replaceFragment(Fragment fragment){
